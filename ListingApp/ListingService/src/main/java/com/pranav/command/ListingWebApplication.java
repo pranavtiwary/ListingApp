053bc7aa4cba8f25d9862c6741f103e3fa94629f
pranav tiwary <tiwaryp@amazon.com>
2021-05-08 13:42:21 +0530

    Spring Boot Intializer Class and gitignore

---
+++ ListingApp/ListingService/src/main/java/com/pranav/command/ListingWebApplication.java
@@ -0,0 +1,15 @@
+package com.pranav.command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListingWebApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ListingWebApplication.class, args);
+
+	}
+
+}
---
+++ ListingApp/UserService/src/main/java/command/UserWebApplication.java
@@ -0,0 +1,15 @@
+package command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserWebApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(UserWebApplication.class, args);
+
+	}
+
+}
